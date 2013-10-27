dca93ca3d3bbd7f6a42b0ebeed8babaa24e60e15
Manuel Huber <manuel.h87@gmail.com>
2013-10-27 16:59:44 +0100

    Minimal changes on example program

--- rpcexample.py
+++ rpcexample.py
@@ -8,6 +8,9 @@ def printBla (s):
     print ("BLA: '%s'" % s)
     return 0
 
+def reply (s):
+    return "Reply to '%s'" % s
+
 def pathlist (dir):
     for i in os.listdir (dir):
         yield os.path.join (dir, i)
@@ -17,12 +20,15 @@ def server_main ():
     srv = pasxmlrpc.RPCServer ("", 10000, "./ca/cert5.pem",
                                "./ca/key5.pem", ca_files)
     srv.register_function (printBla)
+    srv.register_function (reply)
     srv.serve_forever ()
 
-def client_main ():
+def client_main (host=None):
+    if host is None:
+        host = '127.0.0.1'
     try:
         c = pasxmlrpc.RPCProxy (
-            "https://127.0.0.1:10000",
+            "https://%s:10000" % host,
             "./ca/cert2.pem",
             "./ca/key2.pem",
             "./ca/ca_certs2/cert5.pem"
@@ -30,6 +36,7 @@ def client_main ():
         print ("now call...")
         c.printBla ("Hallo from Client!")
         c.printBla ("Bye from Client!")
+        print ("Response: %s" % c.reply ("Bla bla bla"))
     except socket.error as e:
         print ("Connection error : '%s'" % str (e))
     except pasxmlrpc.Fault as e:
@@ -41,7 +48,10 @@ def main (args):
     if args[0] == 'server':
         return server_main ()
     elif args[0] == 'client':
-        return client_main ()
+        host = None
+        if len (args) >= 2:
+            host = args[1]
+        return client_main (host)
     else:
         return -2
 
